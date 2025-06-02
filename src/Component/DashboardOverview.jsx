ae7ef66a2d41d41012b3479255b646b13281db71
Collyns22 <omeihechibuzor@gmail.com>
2025-06-02 14:47:22 +0100

    Modified Dashboard overview component

--- src/Component/DashboardOverview.jsx
+++ src/Component/DashboardOverview.jsx
@@ -4,9 +4,11 @@ import ChartComponent from "../Component/ChartComponent.jsx";
 const DashboardOverview = () => {
   return (
     <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Dashboard Overview</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
+      <div className="mb-4">
         <KPI />
+      </div>
+      <div>
         <ChartComponent />
       </div>
     </div>
